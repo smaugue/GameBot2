a8bfab3ace2a48c1ebb35501ea53b96567f00b7f
Smaugue <ssmaugue@gmail.com>
2024-11-22 11:26:44 +0100

    Initial commit

---
+++ encoder.py
@@ -0,0 +1,12 @@
+def encrypt_token(token, key):
+    encrypted_token = ""
+    for i, char in enumerate(token):
+        decalage = ord(key[i % len(key)])
+        code = (ord(char) + decalage) % 256
+        encrypted_token += str(code) + " "
+    return encrypted_token.strip()
+
+token = input("token=")
+key = input("key=")
+
+print(encrypt_token(token,key))
